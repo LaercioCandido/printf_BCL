a21db12c8b8319d24130520e46d9ab75b97df7aa
rcamiloo <rodrigo.camilo@gmail.com>
2020-07-15 20:01:50 -0300

    modificado o .h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rcamilo- <rcamilo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 21:16:36 by rcamilo-          #+#    #+#             */
-/*   Updated: 2020/07/11 15:38:32 by camilo           ###   ########.fr       */
+/*   Updated: 2020/07/15 19:37:35 by camilo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,14 @@ void	checkflag(const char f, t_flags *flags);
 int		readflag(t_flags *flags, const char *str);
 int		ft_numlen(int num);
 int 	ft_strlen(char *s);
+char	*ft_itoa_base(unsigned long int n, char type);
 int 	ft_printf_d(t_flags *flags, va_list args);
 int		ft_printf_c(t_flags *flags, va_list args);
 int		ft_printf_s(t_flags *flags, va_list args);
 int		ft_printf_x(t_flags *flags, va_list args);
 int		ft_printf_u(t_flags *flags, va_list args);
+int		ft_printf_p(t_flags *flags, va_list args);
+int		ft_printf_pct(t_flags *flags);
 int		ft_printf(const char *str, ...);
 
 #endif
